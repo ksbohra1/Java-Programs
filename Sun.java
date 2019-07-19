dada18f952a4d50d7e1d1544a60bb67bc1fda13c
root <root@localhost.localdomain>
2019-07-18 22:29:13 -0700

    Second commit

--- Sun.java
+++ Sun.java
@@ -1,10 +1,10 @@
 class Sun {
 public static void main(String[] args){
-int a,b,c,d;
+int a,,c,d;
 a=50;
-b=20;
+=20;
 c=30;
 d=a+b+c;
-System.out.println("The sum of three values = " +d);
+System.out.println(The sum of three values = " +d);
 }
 }
